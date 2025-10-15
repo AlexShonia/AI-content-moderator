0c6eafe0484581af459d4776796508ebc278eed1
AlexShonia <alex.shonia123@gmail.com>
2025-10-15 10:20:30 +0400

    integrate supabase with frontend, with auth

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
+
+  const allowedOrigin = process.env.FRONTEND_CORS_ORIGIN;
+  app.enableCors({
+    origin: allowedOrigin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ frontend/components/hero.tsx
@@ -0,0 +1,14 @@
+
+export function Hero() {
+  return (
+    <div className="flex flex-col gap-16 items-center">
+      <div className="flex gap-8 justify-center items-center">
+        <span className="border-l rotate-45 h-6" />
+      </div>
+      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+        AI-Powered Content Moderator
+      </p>
+      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+    </div>
+  );
+}
